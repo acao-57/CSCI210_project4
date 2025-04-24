06dfa12eee3a481cd18d385f6779d8c055a907a0
acao-57 <amycao78@gmail.com>
2025-04-24 10:56:57 +0800

    Update rsh.c

--- rsh.c
+++ rsh.c
@@ -31,12 +31,20 @@ void sendmsg (char *user, char *target, char *msg) {
 	// Send a request to the server to send the message (msg) to the target user (target)
 	// by creating the message structure and writing it to server's FIFO
 
+	struct message m; //creates a message structure
+	
+	//copies the parameter and leaves room for null termination
+	strncpy(m.source, user, sizeof(m.source) - 1);
+	strncpy(m.target, target, sizeof(m.target) - 1);
+	strncpy(m.msg, msg, sizeof(m.msg) - 1);
 
+	//opens the server fifo
+	int fd = open("serverFIFO", O_WRONLY);
 
+	//writes the message to the server
+	write(fd, &m, sizeof(m));
 
-
-
-
+	close(fd); //closes the fifo
 
 }
 
@@ -49,11 +57,36 @@ void* messageListener(void *arg) {
 	// Incoming message from [source]: [message]
 	// put an end of line at the end of the message
 
+	//char fifoName[10];
+	//sprintf(fifoName, "%s", (char*)arg); //uses the same username as the FIFO name
 
+	int fd = open(uName, O_RDONLY);
+	struct message m;
 
+	//creates the infinite loop
+	while (1)
+	{
+		//reads incoming messages
+		ssize_t bytesRead = read(fd, &m, sizeof(m));
 
+		//if there is a message, print the following
+		if (bytesRead > 0)
+		{
+			printf("Incoming message from %s: %s\n", m.source, m.msg);
+		}
 
+		//if we have 0 bytes, the fifo was closed by the writing end so the current message is done
+		//we reopen the reading fifo for other incoming messages
+		if (bytesRead == 0)
+		{
+			//close the pipe and then reopen it for more incoming messages since we have a loop
+			close(fd);
+			fd = open(uName, O_RDONLY);
+		}
+	}
 
+	//just put as a precaution in case i missed something
+	close(fd);
 	pthread_exit((void*)0);
 }
 
@@ -85,7 +118,15 @@ int main(int argc, char **argv) {
 
     // TODO:
     // create the message listener thread
-
+	pthread_t tid;
+	int thread = pthread_create(&tid, NULL, messageListener, (void*)uName); //used slides for help
+    
+	//looks for error in case creation fails
+	if (thread != 0) {
+        perror("Failed to create message listener thread");
+        exit(1);
+    }
+	pthread_detach(tid); //detaches thread for cleanup after exiting
 
 
 
@@ -126,6 +167,23 @@ int main(int argc, char **argv) {
 
 
 
+		//gets the target user, and if null send the following message
+		char *uTarget = strtok(NULL, " ");
+		if(uTarget == NULL)
+		{
+			printf("sendmsg: you have to specify target user\n");
+			continue;
+		}
+
+		//message aka rest of the line, and if null print the following message
+		char *message = strtok(NULL, "");
+		if (message == NULL)
+		{
+			printf("sendmsg: you have to endter a message\n");
+			continue;
+		}
+
+		sendmsg(uName, uTarget, message);
 
 
 
